ca028d9fc8c5a2d630e33b5add6a819922241c8c
ekomuliyo <ekomuliyo@gmail.com>
2019-12-20 21:14:20 +0700

    komentar

---
+++ belajar-go/komentar/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main()  {
+
+	// ini merupakan komentar untuk satu bari
+
+	/*
+		ini merupakan komentar lebih dari satu baris
+	*/
+
+	fmt.Print("Halo Dunia")
+}
