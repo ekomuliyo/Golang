d9e5eb956e5fc4af4e7aae8298487de97572dfe8
eko <ekomuliyo.com>
2020-01-28 20:02:58 +0700

    penggunaan fmt.Print(), fmt.Println()

--- belajar-go/hello-world/main.go
+++ belajar-go/hello-world/main.go
@@ -3,5 +3,13 @@ package main
 import "fmt"
 
 func main() {
-    fmt.Println("hello world")
+	fmt.Println("hello world")
+
+	// penggunaan fmt.Print("")
+	// perlu menambahkan spasi apabila memasukan beberapa nilai
+	fmt.Print("hai", " ", "kamu\n")
+
+	// pegunnan fmt.Println("")
+	// apabila terdapat beberapa nilai di dalamnya otomatis dipisahkan dengan spasi
+	fmt.Println("hai", "juga")
 }
